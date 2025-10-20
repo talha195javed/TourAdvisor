0f71bf6b17a2bd00496690b9aace50afd6d2a98b
MUHAMMAD TALHA JAVED <80463395+talha195javed@users.noreply.github.com>
2025-10-20 11:48:22 +0400

    showing image as slider

--- resources/views/admin/packages/index.blade.php
+++ resources/views/admin/packages/index.blade.php
@@ -173,7 +173,7 @@
                                     Edit Package
                                 </span>
                         </a>
-                        <a href="#" class="text-green-600 hover:text-green-900 transition-colors group relative" title="View">
+                        <a href="{{ route('admin.packages.show', $pkg) }}" class="text-green-600 hover:text-green-900 transition-colors group relative" title="View">
                             <i class="fas fa-eye"></i>
                             <span class="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white text-xs rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap z-10">
                                     View Details
